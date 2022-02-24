6d182127744f9ba8ccaa5c6bdf837575b72def4d
Ben Pedrick <bpedrick@gmail.com>
2022-02-24 13:29:23 -0600

    typo, ignore

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
-        <p>Checkout Total (hold for later></p>
+        <p>Checkout Total (hold for later)</p>
       </header>
   
       {/* <img src='images/pizza_photo.png' />
