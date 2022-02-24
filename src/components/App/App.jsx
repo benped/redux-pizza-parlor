5077220d6bff3b6a947477cd29cee850c96f40b0
Ben Pedrick <bpedrick@gmail.com>
2022-02-24 11:33:35 -0600

    routes added and imports added to app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Pizza from "../Pizza/Pizza.jsx";
+import CustomerForm from "../CustomerForm/CustomerForm.jsx";
+import Checkout from "../Checkout/Checkout.jsx";
 
 function App() {
 
   return (
+    <Router>
+
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
+        <p>Checkout Total (hold for later></p>
       </header>
   
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      {/* <img src='images/pizza_photo.png' />
+      <p>Pizza is great.</p> */}
+        <Route path="/" exact>
+         <Pizza />
+        </Route>
+        <Route >
+          <CustomerForm path="/customerform"/>
+        </Route>
+        <Route>
+          <Checkout path="/checkout"/>
+        </Route>
     </div>
+    </Router>
   );
 }
 
