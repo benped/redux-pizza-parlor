2d86639980e083b50538e3b6bffbc59e1b75f3e8
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-02-24 10:51:45 -0600

    reducers created and added to the store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+// redux stuff
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pizzaReducer = (state = [], action) => {
+    console.log('mmm pizza');
+    return state;
+}
+
+const customerReducer = (state = [], action) => {
+    console.log('I AM THE MANAGER!');
+    return state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+        customerReducer
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
